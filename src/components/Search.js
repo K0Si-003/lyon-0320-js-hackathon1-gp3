0155ab711d7f4d2a5674b5e008705d0ce2364069
wdwcs <Arminit3@gmail.com>
2020-04-29 20:57:24 +0200

    add search.css

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../style/search.css'
 
 const Search = ({search, handleChange, onClick}) => {
   return (
-    <div>
-      <input type='text' placeholder='Find your Jewel' value={search} onChange={handleChange} />
-      <button onClick={onClick}>Search</button>
-    </div>
+      <div className='search'>
+        <input className='searchTerm' type='text' placeholder='Find your Jewel' value={search} onChange={handleChange}/>
+        <button className='searchButton' onClick={onClick}>Search</button>
+      </div>
   )
 }
 
