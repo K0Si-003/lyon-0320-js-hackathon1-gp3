c9cf7bc346395b8660af88d6ec267ba3e122ec92
tmicka23 <mickathp42@gmail.com>
2020-04-29 18:40:32 +0200

    refactor app & tranfer components into home

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
-import Search from './components/Search';
-import ThumbList from './components/ThumbList';
+import Search from "./components/Search";
+import ThumbList from "./components/ThumbList";
 import Axios from "axios";
 
 class App extends React.Component {
@@ -12,50 +12,67 @@ class App extends React.Component {
     super(props);
     this.state = {
       resultsIds: null,
-      results :null,
+      results: null,
       search: "",
-      onSearch: false
-
+      onSearch: false,
     };
   }
 
-   searchIds = async () => {
+  searchIds = async () => {
     const resultsIds = await Axios.get(
       `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${this.state.search}`
     ).then((response) => response.data.objectIDs);
     this.setState({ resultsIds, onSearch: true });
-  }; 
+  };
 
   getResults = async () => {
-    if(this.state.resultsIds === null) {
-        console.log('La nature a horreur du vide !')
-        this.setState({onSearch: false})
+    if (this.state.resultsIds === null) {
+      console.log("La nature a horreur du vide !");
+      this.setState({ onSearch: false });
     } else {
-      const results = await Promise.all(this.state.resultsIds.slice(0, 10).map((id) => Axios.get(
-        `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
-      ).then((response) => response.data))) 
+      const results = await Promise.all(
+        this.state.resultsIds
+          .slice(0, 10)
+          .map((id) =>
+            Axios.get(
+              `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+            ).then((response) => response.data)
+          )
+      );
       console.log(results);
-      this.setState({results})
+      this.setState({ results });
     }
-  }
+  };
 
   handleChange = (event) => {
-    const search =  event.target.value
+    const search = event.target.value;
     this.setState({ search, onSearch: false });
-  }
+  };
 
   render() {
     return (
       <Router>
         <div className="App">
-          <img src={require('./images/Logo.png')} className="logo" alt="Logo Museum Search" />
-          <Search search={this.state.search} handleChange={this.handleChange} onClick={this.searchIds}/>
-          {this.state.onSearch && this.state.resultsIds !== null ? <ThumbList items={this.state.results} method={this.getResults}/> : <p>Error</p>}
-
-        </div>  
+          <img
+            src={require("./images/Logo.png")}
+            className="logo"
+            alt="Logo Museum Search"
+          />
+        </div>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/:id" component={Details} />
+          <Route exact path="/">
+            <Home
+              {...this.state}
+              onLoad={this.getResults}
+              onClick={this.searchIds}
+              handleChange={this.handleChange}
+            />
+          </Route>
+          <Route
+            exact
+            path="/details/:id"
+            render={(routeProps) => <Details {...routeProps} />}
+          />
         </Switch>
       </Router>
     );
