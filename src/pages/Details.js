c39dd20fd15964be3fe4a3cd1ff090c8262c18d1
Riri Z <riri.zamor@gmail.com>
2020-04-30 12:13:50 +0200

    hide date when missing

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -45,7 +45,8 @@ class Details extends Component {
             <section className="info-details">
               <h3>{item.title}</h3>
               <p>{item.repository}</p>
-              <p>Date : {item.objectDate.toString().charAt(0).toUpperCase()+item.objectDate.substring(1).toLowerCase()}</p>
+
+              <p className={(item.objectDate===null)?"showDate": "showDateNone"}>Date : {item.objectDate.toString().charAt(0).toUpperCase()+item.objectDate.substring(1).toLowerCase()}</p>
               <p>{item.dimensions}</p>
               <p>{`${item.geographyType} ${item.country}`}</p>
               <a
